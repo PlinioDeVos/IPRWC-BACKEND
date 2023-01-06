cc3abfae3d6f610b148882606b0807b248d091c4
Plinio <s1127524@student.hsleiden.nl>
2023-01-06 21:39:14 +0100

    :fire: Remove logger

--- src/main/java/nl/plinio/backend/exception/EnitityNotFoundException.java
+++ src/main/java/nl/plinio/backend/exception/EnitityNotFoundException.java
@@ -1,14 +1,11 @@
 package nl.plinio.backend.exception;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@Slf4j
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class EnitityNotFoundException extends RuntimeException {
     public <T> EnitityNotFoundException(Class<T> entity) {
         super("Entity of type '" + entity.getSimpleName() + "' not found.");
-        log.error("Entity of type '" + entity.getSimpleName() + "' not found.");
     }
 }
